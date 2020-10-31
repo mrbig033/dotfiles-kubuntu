f1ec608015a308b7f4e794915f6edb7270cf7215
mrbig033 <mrbig033@protonmail.com>
2020-10-31 13:09:35 -0300

    auto commit

--- maps/scripts/startup.sh
+++ maps/scripts/startup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# hide cursor
+unclutter -idle 1 &
+
+# set keys
 setxkbmap -option -layout br
 xmodmap /home/jones/maps/scripts/xmodmaps
 pkill -fi xcape
 xcape -t 200 -e 'Control_L=Escape'
 xset r rate 200 60
+
+# start emacs server
+emacs --daemon
